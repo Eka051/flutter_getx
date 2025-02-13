fe8929328eea804c59ae9d4366efbd7359fcd5b9
Dian Eka Raharjo <dianekaraharjo@gmail.com>
2025-02-13 22:29:19 +0700

    refactor: enhance product view with detailed list items and navigation

--- lib/app/modules/views/product_view.dart
+++ lib/app/modules/views/product_view.dart
@@ -6,25 +6,29 @@ class ProductView extends StatelessWidget {
   ProductView({super.key});
   final ProductController productController = Get.put(ProductController());
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const 
-        Text('Product List'),
+        title: const Text('Product List'),
       ),
-      body: Obx(() {
-        return ListView.builder(
-          itemCount: productController.product.length,
-          itemBuilder: (context, index) {
-            return ListTile(
-              title: Text(productController.product[index].name),
-              subtitle: Text(productController.product[index].price.toString()),
-            );
-          },
-        );
-      },),
+      body: Obx(
+        () {
+          return ListView.builder(
+            itemCount: productController.product.length,
+            itemBuilder: (context, index) {
+              final product = productController.product[index];
+              return ListTile(
+                title: Text(product.name),
+                subtitle: Text('Rp. ${product.price}'),
+                onTap: () {
+                  Get.toNamed('/product-detail', arguments: product);
+                },
+              );
+            },
+          );
+        },
+      ),
     );
   }
 }
